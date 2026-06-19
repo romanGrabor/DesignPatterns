0157d95c481b5ad9c2e86ab481aef2d4311ec893
romanGrabor <roman13051991@rambler.ru>
2026-06-19 13:36:29 +0300

    Update frontController.php

--- frontController.php
+++ frontController.php
@@ -1,15 +1,31 @@
 <?php
 
 /**
- * Шаблон Front Controller предназначен для создания одной точки входа для всех запросов. Контроллер анализирует URL,
- * заголовки или параметры запроса и передает запрос конкретному обработчику Action/Controller.
- * Результат от Action/Controller возвращается обратно на фронт-контроллер для отправки клиенту.
+ * Шаблон Front Controller предназначен для создания одной точки входа для всех запросов. Front Controller принимает все
+ * входящие HTTP-запросы в одном месте, выполняет общую обработку (маршрутизация, авторизация, логирование, обработка
+ * ошибок и т.д.) и передает управление соответствующему контроллеру или действию (Action).
+ * Контроллер формирует ответ, который затем может быть дополнительно обработан Front Controller и отправлен клиенту.
  * Шаблон корпоративных приложений (Enterprise Patterns).
  */
+
+class UserController
+{
+    public function index(): void {
+        echo 'Список пользователей';
+    }
+}
+
 class FrontController
 {
     public function dispatch(string $route): void
     {
-        echo "Route: $route";
+        switch ($route) {
+            case '/users':
+                (new UserController())->index();
+                break;
+
+            default:
+                echo '404';
+        }
     }
 }
