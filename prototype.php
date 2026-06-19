4727629d6ac25a14291416043638a15a98f3f059
romanGrabor <roman13051991@rambler.ru>
2026-06-19 13:38:42 +0300

    Update prototype.php

--- prototype.php
+++ prototype.php
@@ -2,9 +2,39 @@
 
 /**
  * Шаблон Prototype предназначен для создания объекта через копирование. Полезен при работе с тяжелыми объектами.
+ * Если свойства исходного объекта содержат ссылки на другие объекты, то их значения не клонируются, а будут содержать
+ * ссылки на эти же объекты. Для исключения данной ситуации в объекте необходимо реализовать метод __clone, где данные
+ * свойства принудительно клонируются.
  * Шаблон генерации объектов.
  */
 class UserPrototype
 {
     public string $name;
+    public User $user;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+        $this->user = new User('1', 'Алиса');
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    public function __clone(): void
+    {
+        $this->user = clone $this->user;
+    }
 }
